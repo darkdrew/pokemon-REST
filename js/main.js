fddc52a691aa30834179eea26635c12b34050914
darkdrew <andrewleong14@gmail.com>
2017-09-04 22:04:01 +0800

    changed interface

--- js/main.js
+++ js/main.js
@@ -13,29 +13,12 @@
 
 
     this.getPokemon = function() {
-      var data = [];
-        // var http = new XMLHttpRequest();
-        // var url = `https://pokeapi.co/api/v1/pokemon/charmander/`;
-        //
-        // var method = "GET";
-        // http.open(method, url);
-        //
-        // http.onreadystatechange = function() {
-        //     if(http.readyState === XMLHttpRequest.DONE && http.status === 200) {
-        //       var json = JSON.parse(http.response);
-        //       displayPokemon(json);
-        //       // console.log(json);
-        //     } else if (http.readyState === XMLHttpRequest.DONE && http.status !== 200) {
-        //       alert("Error");
-        //     }
-        // }
-        // http.send();
-        const urls = [`https://pokeapi.co/api/v1/pokemon/charmander/`, `https://pokeapi.co/api/v2/pokemon/charmander/`];
 
-        // var url = `https://pokeapi.co/api/v1/pokemon/charmander/`;
-        var url2 = `https://pokeapi.co/api/v2/pokemon/charmander/`;
+        // API url V1 and V2
+        const urls = [`https://pokeapi.co/api/v1/pokemon/squirtle/`, `https://pokeapi.co/api/v2/pokemon/squirtle/`];
 
-
+        //Higher Scope for passing results to pass down promise chain
+        let result1;
 
         Promise.all(urls.map( (url) => {
             return fetch(url);
@@ -49,149 +32,144 @@
           )
         })
         .then( (result) => {
-            // const descriptionURI = "http://pokeapi.co" + result[0].descriptions[0].resource_uri;
-            // urls.push(descriptionURI);
-            // displayPokemon(result);
-            // console.log(descriptionURI);
-
-            // return fetch(descriptionURI);
-
-            data["results"] = result;
-            displayPokemon(data);
+            let descriptionURI = "https://pokeapi.co" + result[0].descriptions[0].resource_uri;
+            result1 = result;
+            return fetch(descriptionURI);
         })
-        // .then ( (descriptions,result) => {
-        //     console.log(descriptions);
-        //     console.log("results " + result);
-        // })
+        .then ( (response2) => {
+            return response2.json();
+        })
+        .then( (result2) => {
+            result1.push(result2);
+            displayPokemon(result1);
+        })
         .catch( (error) => {
-          alert(error);
-        })
-        console.log(result);
+          console.log(error);
+        });
 
-
-        // console.log(data);
-
-
-          // displayPokemon(data);
-
-          //
-          // fetch(descriptionURI)
-          // .then(function(response) {
-          //     return response.json();
-          //   })
-          // .then(function(json) {
-          //
-          //     // console.log("Description URI: ", data);
-          //     // console.log("Description: ", pokeDescription);
-          //
-          //   })
-          // .catch(function(err){
-          //       alert(err);
-          //   });
-
-
-          // fetch(url2, {method:'get'}).then(function(response) {
-          //     return response.json();
-          //   }).then(function(json3) {
-          //     var imageURI = json3.sprites.front_default;
-          //     // data.push({"image": imageURI});
-          //     // console.log(imageURI);
-          //   }).catch(function(err){
-          //       alert(err);
-          //   });
-          // // console.log(data);
-
-
-
-
-    };
+    }; //End of GetPokemon
 
     this.displayPokemon = (json) => {
-        var content_container = document.querySelector("#content-container");
-        var template = "";
+        const content_container = document.querySelector("#content-container");
+        let template = "";
 
-      //       for(var i in data){
-      //         if (data.hasOwnProperty(i)) {
-      //       console.log(i, data[i]);
-      //   }
-      // }
+      let pokeID = json[0].national_id;
+      let pokeName = json[0].name;
+      let pokeType1 = json[0].types[0].name;
+      let imageURI = json[1].sprites.front_default;
+      let pokeDescription = json[2].description;
+      let pokeAttack = json[0].attack;
+      let pokeDefense = json[0].defense;
 
-      // const pokeID = json[0].national_id;
-      // const pokeName = json[0].name;
-      // const pokeType1 = json[0].types[0].name;
-      // if (json[0].types.length == 2) {
-      //     const pokeType2 = json[0].types[1].name;
-      //   } else {
-      //     const pokeType2 = null;
-      //   }
+      let pokeAbilities = json[0].abilities.map( (item)=> item.name );
+      let pokeEggGroup = json[0].egg_groups.map( (item)=> item.name );
+
+      let pokeEvolution = json[0].evolutions;
+      let pokeEvolutionTo = [...new Set(pokeEvolution.map( (item)=> item.to ) )];
+      let pokeEvolutionLevel = [...new Set(pokeEvolution.map( (item)=> item.level ) )];
+      let pokeEvolutionMethod = [...new Set(pokeEvolution.map( (item)=> item.method ) )];
+
+        if(pokeEvolution.length === 0){
+            pokeEvolutionTo = null;
+            pokeEvolutionLevel = null;
+            pokeEvolutionMethod = null;
+        }else if(!pokeEvolutionTo){
+            pokeEvolutionTo = null;
+        }else if (typeof pokeEvolutionLevel[0] === 'undefined' || pokeEvolutionLevel === null){
+            pokeEvolutionLevel = null;
+        }
 
 
-      // let pokeDescription = "";
+        let pokeType2;
+        if (json[0].types.length === 2) {
+            pokeType2 = json[0].types[1].name;
+          } else {
+            pokeType2 = null;
+          }
 
-      console.log(json['results'][0]);
+      let pokeMoves = json[0].moves.map( (item) => item.name);
+
+      console.log(pokeID,pokeName,pokeType1, pokeType2, pokeDescription, pokeAttack, pokeDefense ,pokeAbilities, pokeEggGroup,pokeEvolutionTo, pokeEvolutionTo ,pokeEvolutionLevel,pokeEvolutionMethod,imageURI, pokeMoves);
+      console.log(json);
+
+      template +=`
+      <div class="col col-md-4">
+
+        <div class="col-md-12 text-center box"><img src="${imageURI}"></div>
+
+        <div class="col-md-12 text-center border-list"># ${pokeID}</div>
+        <div class="col-md-12 text-center border-list">${pokeName}</div>
+        <div class="col-md-12 text-center border-list">Type1: ${pokeType1}</div>
+        <div class="col-md-12 text-center border-list">Type2: ${pokeType2}</div>
+
+      </div>
 
 
+      <div class="col">
+          <div class="col-md-12">
+              <div class="card" style="margin-bottom:10px;">
+                <div class="card-block">
+                  <div class="card-text">Description: ${pokeDescription}</div>
+                </div>
+              </div>
+          </div>
 
 
-        // for(var i = 0; i < json.length; i++){
-        //       // template += '<div class="card_list">'+ json[i].name +'</div>';
-        //       // template += '<div style="background:url('+ json.sprites[i].resource_uri +');"</div>';
-        // }
+          <div class="col-md-12">
+                <div class="card" style="margin-bottom:10px;">
+                  <div class="card-block">
+                    <div class="card-text">Species: ${pokeEggGroup}</div>
+                  </div>
+                  <div class="card-block">
+                    <div class="card-text">Abilities: ${pokeAbilities}</div>
+                  </div>
+                </div>
+          </div>
 
-        // template += '<div>'+ pokeName +'</div>';
-        //
-        // console.log(`name: ${json.name}`);
-        // console.log(`ID: ${json.national_id}`);
-        // console.log(`Type: ${json.types[i].name}`);
-        // console.log(json.descriptions[0].resource_uri);
-        // console.log(json);
+
+        <div class="col-md-12">
+          <div class="card" style="margin-bottom:10px;">
+            <div class="row">
+              <div class="col-md-6 card-block">Attack: ${pokeAttack}</div>
+              <div class="col-md-6 card-block">Defense: ${pokeDefense}</div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-12">
+          <div class="card" style="margin-bottom:10px;">
+            <div class="card-block">Evolve to: ${pokeEvolutionTo}</div>
+            <div class="card-block">Evolve level: ${pokeEvolutionLevel}</div>
+            <div class="card-block">Evolve method: ${pokeEvolutionMethod}</div>
+            <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#flipFlop">List of Moves</button>
+
+          </div>
+        </div>
+
+      </div>
+      `;
+
+      template +=`
+      <div class="modal fade" id="flipFlop" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+           <div class="modal-content">
+             <div class="modal-header">
+               <h5 class="modal-title" id="exampleModalLabel">List of Moves</h5>
+               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                 <span aria-hidden="true">&times;</span>
+               </button>
+             </div>
+             <div class="modal-body">${pokeMoves}</div>
+           </div>
+          </div>
+      </div>
+      `;
 
         content_container.innerHTML = "";
         content_container.insertAdjacentHTML("afterbegin", template);
-
     };
+
+    
 
     this.init();
 })();
-
-//
-// function pokeSubmit(){
-//     var param = document.getElementById("pokeInput").value;
-//     var pokeURL = "http://pokeapi.co/api/v1/pokemon/" + param;
-//
-//     // new URL for 3rd GET request
-//     var pokeURL2 = "http://pokeapi.co/api/v2/pokemon/" + param;
-//
-//     $.getJSON(pokeURL, function(data){
-//         //console.log(data);
-//         var pokeID = data.national_id;
-//         var pokeName = data.name;
-//         var pokeType1 = data.types[0].name;
-//         if (data.types.length == 2) {
-//             var pokeType2 = data.types[1].name;
-//         }
-//         else var pokeType2 = null;
-//         var descriptionURI = "http://pokeapi.co" + data.descriptions[0].resource_uri;
-//         var pokeDescription = "";
-//
-//         $.getJSON(descriptionURI, function(data2){
-//             //console.log(data2);
-//             pokeDescription = data2.description;
-//         });
-//
-//         // 3rd GET request to get an image
-//         $.getJSON(pokeURL2, function(data3){
-//             console.log(data3);
-//             console.log(JSON.stringify(data, null, "  "));
-//             /*
-//             console.log("Number: ", pokeID);
-//             console.log("Name: ", pokeName);
-//             console.log("Type 1: ", pokeType1);
-//             console.log("Type 2: ", pokeType2);
-//             console.log("Description URI: ", descriptionURI);
-//             console.log("Description: ", pokeDescription);
-//             */
-//         });
-//
-//     });	// 2nd and 3rd GET requests are nested in success function of 1st GET request
-// }
